d95c884da3f2f28b2de1df78ff56764b21514c03
Hernan <hernan24744@gmail.com>
2024-07-19 23:42:17 -0300

    homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
 <template>
-  <HelloWorld />
+  <div class="mb-28 min-h-screen flex items-center justify-center">
+    <TituloSeccion
+      class="coworks xs:text-7xl sm:text-8xl md:text-9xl"
+      titulo="Coworks"
+    />
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from "@/components/HelloWorld.vue";
+import TituloSeccion from "../components/TituloSeccion.component.vue";
 </script>
+
+<style scoped>
+.coworks {
+  text-shadow: 0px 0px 14.2px rgba(255, 255, 255, 0.75);
+}
+</style>
